51421c0ce19a480489d5bea57635bea3a29d8b2e
Dustin <dustin@ogc.engineering>
2023-06-19 13:11:20 -0500

    add reference to source repos and start Makefile

---
+++ deployment.h
@@ -0,0 +1,9 @@
+#ifndef DEPLOYMENT_H
+#define DEPLOYMENT_H
+
+#include <stdint.h>
+
+/* redefine the number of elements in the event tracking array */
+#define OGC_LIB_OS_NRTPS__EVENT_ARRAY_SIZE ( ( uint8_t )3 )
+
+#ifndef /* DEPLOYMENT_H */
